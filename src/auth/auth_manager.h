6015c951e15da6029330748869a846c679184a62
Bekix <Dimentr65@mail.ru>
2026-05-16 15:56:57 +0300

    fix

--- src/auth/auth_manager.h
+++ src/auth/auth_manager.h
@@ -1,60 +1,31 @@
 #pragma once
-#include <windows.h>
 #include <string>
-
+#include <windows.h>
 #include "../common/rpc_status.h"
 
+struct AuthState
+{
+    bool isAuthenticated = false;
+    std::wstring username;
+    std::wstring accessToken;
+    std::wstring refreshToken;
+    bool hasLicense = false;
+    std::wstring licenseTicket;
+    long long accessExpiresAt = 0;
+    long long refreshExpiresAt = 0;
+    long long licenseExpiresAt = 0;
+};
+
+// Глобальные переменные объявляются в cpp, здесь только extern
+extern CRITICAL_SECTION g_authLock;
+extern AuthState g_state;
+
+// Интерфейс
 void AuthInitialize();
 void AuthShutdown();
-
 int AuthGetCurrentUser(bool* isAuthenticated, std::wstring* username);
-
-//int AuthLogin(const std::wstring& login, const std::wstring& password);
-int AuthLogin(const std::wstring& login, const std::wstring& password)
-{
-    if (login != L"admin" || password != L"12345")
-    {
-        return RPC_ERROR_INVALID_CREDENTIALS;
-    }
-
-    EnterCriticalSection(&g_authLock);
-
-    g_state.isAuthenticated = true;
-    g_state.username = login;
-
-    g_state.accessToken = L"mock_access";
-    g_state.refreshToken = L"mock_refresh";
-
-    g_state.accessExpiresAt = std::time(nullptr) + 3600;
-    g_state.refreshExpiresAt = std::time(nullptr) + 7200;
-
-    LeaveCriticalSection(&g_authLock);
-
-    return RPC_OK;
-}
+int AuthLogin(const std::wstring& login, const std::wstring& password);
 int AuthLogout();
-
+int AuthActivateProduct(const std::wstring& activationCode);
 int AuthGetLicenseInfo(bool* hasLicense, long long* expiresAtUnix);
-
-//int AuthActivateProduct(const std::wstring& activationCode);
-int AuthActivateProduct(const std::wstring& activationCode)
-{
-    if (activationCode != L"MTUCI-2025")
-    {
-        return RPC_ERROR_ACTIVATION_FAILED;
-    }
-
-    EnterCriticalSection(&g_authLock);
-
-    g_state.hasLicense = true;
-    g_state.licenseTicket = L"mock_ticket";
-
-    g_state.licenseExpiresAt =
-        std::time(nullptr) + 30 * 24 * 60 * 60;
-
-    LeaveCriticalSection(&g_authLock);
-
-    return RPC_OK;
-}
-
 bool AuthHasLicense();
