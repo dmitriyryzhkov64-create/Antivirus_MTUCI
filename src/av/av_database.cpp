5b638aa6e3aa0762df0906270b14501f682a33c0
Bekix <Dimentr65@mail.ru>
2026-05-23 16:57:09 +0300

    Restrict AV signature offsets

--- src/av/av_database.cpp
+++ src/av/av_database.cpp
@@ -46,7 +46,7 @@ void AvDatabase::LoadMockDatabase()
         record.objectSignatureLength = static_cast<uint32_t>(signature.size());
         record.objectSignatureHash = SimpleHash(signature);
         record.offsetBegin = 0;
-        record.offsetEnd = 1024 * 1024;
+        record.offsetEnd = 64;
         record.objectType = AvObjectType::PE;
         record.avRecordSignature = { 0xAA, 0xBB, 0xCC };
         record.threatName = L"Test.EICAR.Mock";
